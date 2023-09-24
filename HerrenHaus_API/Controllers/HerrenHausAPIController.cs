ba220878646eb72c1af79fabbb667989befdccd9
SumayaAli <Sumaya.Jalal.Ali@gmail.com>
2023-09-24 13:35:23 +0300

    Implementing HTTPPost, ActionResult & StatusCode

--- HerrenHaus_API/Controllers/HerrenHausAPIController.cs
+++ HerrenHaus_API/Controllers/HerrenHausAPIController.cs
@@ -1,6 +1,7 @@
 ﻿using HerrenHaus_API.Data;
 using HerrenHaus_API.Models;
 using HerrenHaus_API.Models.Dto;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq.Expressions;
 
@@ -12,16 +13,52 @@ namespace HerrenHaus_API.Controllers
     [ApiController]
     public class HerrenHausAPIController:ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<HerrenHausDto> GetAllHerrenHaus()
+        [HttpGet()]
+        //[ProducesResponseType(200)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<HerrenHausDto>> GetAllHerrenHaus()
         {
-            return HerrenHausStore.HerrenHausList;
+            return Ok(HerrenHausStore.HerrenHausList);
         }
       //  [HttpGet("id")]
-        [HttpGet("{id:int}")]
-        public HerrenHausDto GetHerrenHausByID(int id)
+        [HttpGet("{id:int}",Name ="GetHerrenHausByID")]
+        //[ProducesResponseType(200,Type=typeof(HerrenHausDto))]
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<HerrenHausDto> GetHerrenHausByID(int id)
         {
-            return HerrenHausStore.HerrenHausList.FirstOrDefault(u=>u.ID==id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var Haus= HerrenHausStore.HerrenHausList.FirstOrDefault(u => u.ID == id);
+            if(Haus == null)
+            {
+                return NotFound();
+            }
+            return Ok(Haus);
+        }
+        [HttpPost()]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<HerrenHausDto> CreateHerrenHaus([FromBody] HerrenHausDto herrenHausDto)
+        {
+            if(herrenHausDto == null)
+            {
+                return BadRequest(herrenHausDto);
+            }
+            if(herrenHausDto.ID > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            herrenHausDto.ID = HerrenHausStore.HerrenHausList.OrderByDescending(u => u.ID).FirstOrDefault().ID + 1;
+            HerrenHausStore.HerrenHausList.Add(herrenHausDto);
+            //to provide a link for the created object - return 201
+            return CreatedAtRoute("GetHerrenHausByID", new { id = herrenHausDto.ID },herrenHausDto);
         }
     }
 }
--- HerrenHaus_API/Data/HerrenHausStore.cs
+++ HerrenHaus_API/Data/HerrenHausStore.cs
@@ -4,7 +4,7 @@ namespace HerrenHaus_API.Data
 {
     public static class HerrenHausStore
     {
-        public static IEnumerable< HerrenHausDto> HerrenHausList = new List<HerrenHausDto>()
+        public static List< HerrenHausDto> HerrenHausList = new List<HerrenHausDto>()
             {
                 new HerrenHausDto { ID=1,Name="HerrenHaus am Strand"},
                 new HerrenHausDto { ID=2,Name="HerrenHaus im Berg"}
